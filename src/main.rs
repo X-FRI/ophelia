8ad7cb9e67798002f34ec1c28ef2f9ec9d2e8b8d
Muqiu Han <muqiu-han@outlook.com>
2023-09-22 10:17:42 +0800

    [Add]: A simple parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use lalrpop_util::lalrpop_mod;
+use std::env::args;
+use std::fs::read_to_string;
+use std::io::Result;
+
+lalrpop_mod!(ophelia);
+
+fn main() -> Result<()> {
+    let mut args = args();
+
+    args.next();
+    let _mode = args.next().unwrap();
+    let input = args.next().unwrap();
+
+    args.next();
+    // let _output = args.next().unwrap();
+
+    let input = read_to_string(input)?;
+    let ast = ophelia::ProgramParser::new().parse(&input).unwrap();
+
+    println!("{}", ast);
+
+    Ok(())
 }
